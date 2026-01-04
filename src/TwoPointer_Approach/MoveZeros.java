04094cf93869079c6efc61f1500a949c75b27119
Sauraj Singh <saurajsingh337@gmail.com>
2026-01-04 21:52:23 +0530

    first commit

---
+++ src/TwoPointer_Approach/MoveZeros.java
@@ -0,0 +1,42 @@
+package TwoPointer_Approach;
+
+import java.util.Arrays;
+
+public class MoveZeros {
+
+    public static void moveZerosAtLast(int[] arr){
+
+        int j = 0;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    public static void moveZerosAtFirst(int[] arr){
+
+        int j = 0;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,0,6,0,8,0,1,2,0};
+
+        System.out.println("Before " + Arrays.toString(arr));
+        moveZerosAtFirst(arr);
+        System.out.println("After " + Arrays.toString(arr));
+
+    }
+
+}
